3eec69c99b209498cc52b213e5bd3d7dbf7841ec
Hawa Abukar <Hawaabullahi99@gmail.com>
2022-10-19 10:32:01 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import Showcase from './components/Showcase';
+
 
 function App() {
   return (
     <>
 
+      <Navbar />
+      <Showcase />
+    
     </>
   );
 }
